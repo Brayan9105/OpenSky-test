359d58e5a64d62353dd4aa84a06cd5ba54cd71fa
Brayan lopez <brayan9105@hotmail.com>
2020-10-15 16:14:20 -0500

    Added authorSort class method to Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,13 @@
 class Author < ApplicationRecord
   belongs_to :country
+
+  validates :name, :age, presence: true
+
+  def self.authorSort(param)
+    if param == "edad" || param == "-edad"
+      all.order(age: "#{param == "edad" ? 'ASC' : 'DESC'}".to_sym)
+    else
+      all
+    end
+  end
 end
