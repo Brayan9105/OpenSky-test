7afceb1fe3b6504f12f8e61a2ab81519055c6937
Brayan lopez <brayan9105@hotmail.com>
2020-10-15 16:30:15 -0500

    Added post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_211711) do
+ActiveRecord::Schema.define(version: 2020_10_15_212829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,5 +38,15 @@ ActiveRecord::Schema.define(version: 2020_10_15_211711) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
   add_foreign_key "authors", "countries"
+  add_foreign_key "posts", "authors"
 end
