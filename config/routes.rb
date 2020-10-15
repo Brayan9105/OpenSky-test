45c255091655cf56bf1f3325df92740233693256
Brayan lopez <brayan9105@hotmail.com>
2020-10-15 16:37:27 -0500

    Added authorPost view and route + validations in the model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
+  
   get '/logs', to: 'applogs#index'
+
   resources :authors
+  get '/posts/:id/author', to: 'posts#authorPost', as: 'authorPost'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
