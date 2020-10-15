abee96de0336f16668f875279ebea6632bc3f7f7
Brayan lopez <brayan9105@hotmail.com>
2020-10-15 17:45:14 -0500

    Added posts api controller methods

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,55 @@
 module Api
   module V1
     class PostsController < ApplicationController
+      before_action :set_post, only: [:show, :update, :destroy]
+
+      def index
+        posts = Post.all
+        render json: posts
+      end
+
+      def show
+        render json: @post
+      end
+
+      def update
+        if @post.update(post_params)
+          Applog.save_log params[:controller], params[:action], @post.id
+          head :no_content
+        else
+          render json: {errors: @post.errors}, status: 422
+        end
+      end
+
+      def create
+        post = Post.new(post_params)
+
+        if post.save
+          Applog.save_log params[:controller], params[:action], post.id
+          head :no_content
+        else
+          render json: {errors: post.errors}, status: 422
+        end
+      end
+
+      def destroy
+        if @post.destroy
+          Applog.save_log params[:controller], params[:action], @post.id
+          head :no_content
+        else
+          render json: {errors: @post.errors}, status: 422
+        end
+      end
+
+      private
+
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      def post_params
+        params.require(:post).permit(:title, :content, :author_id)
+      end
     end
   end
 end
