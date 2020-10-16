822f022fe1c693b6e3c01ecce7b3bb18d08e31d2
Brayan lopez <brayan9105@hotmail.com>
2020-10-16 07:59:58 -0500

    Added author model test + seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+country = ['Colombia', 'Venezuela', 'Argentina', 'Brazil', 'Chile', 'Ecuador', 'Bolivia']
+country.each do |country|
+  Country.create(name: country)
+end
+
+Author.create(
+  [
+    {name: 'Sara', age: 33, country_id: 1},
+    {name: 'Armando', age: 28, country_id: 2},
+    {name: 'Felicia', age: 42, country_id: 3},
+    {name: 'Karen', age: 30, country_id: 4}
+  ]
+)
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate' do
+    context "when author does not have all fields" do
+      it 'should return false' do
+        expect(Author.new.valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Author.new(name: 'Juan').valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Author.new(age: 33).valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Author.new(country_id: 1).valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Author.new(name: 'Juan', age: 33).valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Author.new(name: 'Juan', country_id: 1).valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Author.new(age: 33, country_id: 1).valid?).to be_falsey 
+      end
+    end# End context
+
+    context 'when author has all fields' do
+      Country.create(name: 'Colombia')
+
+      it 'should return true' do
+        expect(Author.new(name: 'as', age: 33, country_id: 1).valid?).to be_truthy
+      end
+    end# End context
+  end# End describe
 end
