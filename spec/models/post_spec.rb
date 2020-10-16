6ed1db31e7670aab26b5bbda7aa5b0c02dad7ba5
Brayan lopez <brayan9105@hotmail.com>
2020-10-16 08:10:53 -0500

    Added post model test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  
+  describe 'validate' do
+    Country.create(name: 'Colombia')
+    Author.create(name: 'Sara', age: 33, country_id: 1)
+
+    context "when post does not have all fields" do
+      it 'should return false' do
+        expect(Post.new.valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Post.new(title: 'My post').valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Post.new(content: 'Some content').valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Post.new(author_id: 1).valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Post.new(title: 'My post', content: 'My content').valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Post.new(title: 'My post', author_id: 1).valid?).to be_falsey 
+      end
+
+      it 'should return false' do
+        expect(Post.new(content: 'My content', author_id: 1).valid?).to be_falsey 
+      end
+    end# End context
+
+    context 'when post has all fields' do
+      it 'should return true' do
+        expect(Post.new(title: 'My post', content: 'My content', author_id: 1).valid?).to be_truthy 
+      end
+    end# End context
+  end# End describe
 end
